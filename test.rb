fbf4f3f0cc18c359031d77fd9af4b067642e81dd
jlambert8851 <james.lambert@kellogg.northwestern.edu>
2020-05-19 16:26:24 +0000

    got news to sort of work

--- test.rb
+++ test.rb
@@ -1,9 +1,5 @@
-require "sinatra"
-require "sinatra/reloader"
 require "httparty"
-def view(template); erb template.to_sym; end
 
-get "/" do
   ### Get the weather
   # Evanston, Kellogg Global Hub... replace with a different location if you want
   lat = 42.0574063
@@ -23,19 +19,13 @@ get "/" do
   @news = HTTParty.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=15bd741879c5480bb685e21946571cd9").parsed_response.to_hash
 
   ###Some arrays to loop through
-  @articles = [0,1,2,3,4,5,6,7]
+  @articles = [0,1,2,3,4,5,6,7] 
   @days = [0,1,2,3,4]
-
   @headlines = []
-  @urls = []
 
-for no in @articles
-  @headlines << @news["articles"][no]["title"]
-end
-
-for no in @articles
-  @urls << @news["articles"][no]["url"]
-end
-
-  view "news"
-end
+#for no in @articles
+#  puts @news["articles"][no]["title"]
+#end
+ 
+for day in @days
+ puts @forecast["current"]["weather"][0]["description"]
